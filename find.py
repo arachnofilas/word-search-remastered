ddc1d60499c4c5f7a3ada6757f77740bc9015570
Dziugas <d.mikalkenas@gmail.com>
2017-03-29 19:30:36 +0300

    Added argparser to find.py

--- find.py
+++ find.py
@@ -1,24 +1,31 @@
 #!/usr/bin/python3.4
 
+import sys
+import argparse
 from validate import Validator
 from search import fileReader
 
 class Boss:
 
     def __init__(self):
-        print('Boss created')
+        self.validator = Validator()
+        self.reader = fileReader()
 
-    def execute(self):
-        validator = Validator()
-        reader = fileReader()
-        validator.ensureWordIsValid()
-        importedFile = validator.ensureFileIsValid()
-        print(reader.prepareFile(importedFile))
+    def execute(self, filename, word):
+        self.validator.ensureFileIsValid(filename)
+        myFile = self.reader.readFile(filename)
+        print(self.reader.prepareFile(myFile))
+
+def main():
+    parser = argparse.ArgumentParser(description='This CLI tool searches ' +
+    'for words in a specified file using Soundex algorithm.')
+    parser.add_argument('filename', metavar='filename', type=str,
+                        help='file to be searched in')
+    parser.add_argument('word', metavar='word', type=str,
+                        help='word to search for')
+    args=parser.parse_args()
+    executor = Boss()
+    executor.execute(args.filename,args.word)
 
 if __name__ == '__main__':
-    executor = Boss()
-    executor.execute()
-
-    '''
-    TO DO: check if possible create validator and reader objects in init
-    '''
+    main()
